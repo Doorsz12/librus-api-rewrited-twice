b94e3c41e0baaef2d8f63706aa12dda20b603570
Doorsz12 <jas.klocek@gmail.com>
2026-01-30 22:12:34 +0100

    patched pointGrades.js

--- API/Grades/PointGrades.js
+++ API/Grades/PointGrades.js
@@ -3,54 +3,54 @@ class PointGradesAPI {
         this.session = session
       }
 
-    async getPointGrades(headers) {
+    async getPointGrades() {
         try {
-            const response = await this.session.client.get(`${this.session.api.url}/PointGrades`, { headers });
+            const response = await this.session.client.get(`${this.session.api.url}/PointGrades`);
             return response.data;
         } catch (error) {
             console.log("[LibrusAPI: PointGrades] School doesn't support this function");
         }
     }
 
-    async getPointGradesCategories(headers) {
+    async getPointGradesCategories() {
         try {
-            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/Categories`, { headers });
+            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/Categories`);
             return response.data;
         } catch (error) {
             console.log("[LibrusAPI: PointGradesCategories] School doesn't support this function");
         }
     }
 
-    async getPointGradesDictionaries(headers) {
+    async getPointGradesDictionaries() {
         try {
-            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/Dictionaries`, { headers });
+            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/Dictionaries`);
             return response.data;
         } catch (error) {
             console.log("[LibrusAPI: PointGradesDictionaries] School doesn't support this function");
         }
     }
 
-    async getPointGradesAverages(headers) {
+    async getPointGradesAverages() {
         try {
-            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/Averages`, { headers });
+            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/Averages`);
             return response.data;
         } catch (error) {
             console.log("[LibrusAPI: PointGradesAverages] School doesn't support this function");
         }
     }
 
-    async getPointGradesStudentsAverages(headers) {
+    async getPointGradesStudentsAverages() {
         try {
-            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/StudentsAverages`, { headers });
+            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/StudentsAverages`);
             return response.data;
         } catch (error) {
             console.log("[LibrusAPI: PointGradesAverages] School doesn't support this function");
         }
     }
 
-    async getPointGradesCategoriesAverages(headers) {
+    async getPointGradesCategoriesAverages() {
         try {
-            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/CategoriesAverages`, { headers });
+            const response = await this.session.client.get(`${this.session.api.url}/PointGrades/CategoriesAverages`);
             return response.data;
         } catch (error) {
             console.log("[LibrusAPI: PointGradesAverages] School doesn't support this function");
